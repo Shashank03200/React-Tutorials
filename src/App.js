c817bdf28f27f8fc67132fbdee0e94842adb646e
Shashank03200 <shashank032000@gmail.com>
2020-11-27 00:18:18 +0530

    Hooks basics

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-import React, { Component } from 'react'; 
+import React, { useState } from 'react'; 
 import Person from './Person/Person'
 import './App.css';
 import person from './Person/Person';
 
-class App extends Component {
+const App = (props) => {
 
-  state = {
+  const [stateArr,changeStateArray] =  useState({
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age:26}
     ],
     otherState: 'some other value'
-  }
+  })
 
-  switchNameHandler = ()=>{
+    console.log(stateArr);
+
+    const [otherState, setOtherState] = useState('some other value');
+
+  const switchNameHandler = ()=>{
     console.log('Was Clicked');
-    this.setState({
+    changeStateArray({
       persons:[
         {name:"Maximilian", age: 28},
         {name:"Manu", age: 29},
         {name:"Stephanie", age:27}
       ],
+      otherState:otherState
     })
   }
-
-  render() {
+  
     return (
       <div className="App">
         <h1>Hi, I'm A React App</h1>
         <p>This is really working</p>
-        <button onClick = {this.switchNameHandler}>Switch Button</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick = {switchNameHandler}>Switch Button</button>
+        <Person name={stateArr.persons[0].name} age={stateArr.persons[0].age}/>
+        <Person name={stateArr.persons[1].name} age={stateArr.persons[1].age}/>
+        <Person name={stateArr.persons[2].name} age={stateArr.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, I\'m a react app!') );
-  }
+  
 }
 
 export default App;
